33738979f0bbdd8586d539cef27725a7f3b7d6ae
Toufiqul Islam Noyon <45868123+Toufiqul-Islam-Noyon@users.noreply.github.com>
2021-06-24 17:47:16 +0600

    Updated file

--- ETender/settings.py
+++ ETender/settings.py
@@ -142,9 +142,9 @@ MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noyon.socialmedia00@gmail.com'
-DEFAULT_FROM_EMAIL = 'noyon.socialmedia00@gmail.com'
-EMAIL_HOST_PASSWORD = 'socialmedia'
+EMAIL_HOST_USER = ''
+DEFAULT_FROM_EMAIL = ''
+EMAIL_HOST_PASSWORD = ''
 
 ENABLE_USER_ACTIVATION = True
 DISABLE_USERNAME = False
